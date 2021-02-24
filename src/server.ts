14e7c16d43ea342a741209c814d8c5010be25c5b
matheus <matheusfr.318@gmail.com>
2021-02-24 16:43:55 -0300

    Model finished

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message: "HelloWorld - TypeScript"});
-});
-
-app.post("/", (req, res) => {
-    return res.json({message: "Dados enviados com sucesso"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000,() => {
     console.log("Server running on port 3000");
